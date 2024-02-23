10cd342d75d76fec21d1a4185daae6de22f8c146
onyx <100087081+onyx-777@users.noreply.github.com>
2024-02-24 03:09:04 +0530

    final changes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
       remotePatterns: [
         {
-          hostname: "localhost",
+          hostname: ["localhost", "digitalhippo-production-58ed.up.railway.app"],
           pathname: '**',
           port: '3000', // Corrected to a string
           protocol: 'http'
