c82e5fead9fbc4ff61cb06cf3656741806d7f80f
onyx <100087081+onyx-777@users.noreply.github.com>
2024-02-24 05:37:53 +0530

    finalized commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
-// next.config.js
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          hostname: "localhost",
-          pathname: '**',
-          port: '3000', // Corrected to a string
-          protocol: 'http'
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: '**',
+        port: '3000', // Corrected to a string
+        protocol: 'http'
+      },
+      {
+        hostname: "digitalhippo-production-58ed.up.railway.app",
+        pathname: '**',
+        protocol: 'https'
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
